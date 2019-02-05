7b18766840dbefaf28b8697db0f103a1494306ad
GhanDeveloper <GDg11104@gmail.com>
2019-02-06 00:59:52 +0530

    featureV 0.1 /user/signup Api Point is implemented

--- quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDetailsResponse.java
+++ quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDetailsResponse.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * UserDetailsResponse
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-01-29T14:01:42.626+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-02-05T23:45:55.328+05:30")
 
 public class UserDetailsResponse   {
   @JsonProperty("first_name")
