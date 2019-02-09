d6a62ae7ad8dd7dafdb7b3619dff699fdc5f2bc7
savinngr <savinngr@gmail.com>
2019-02-09 14:10:24 +0530

    checkin for following two end points /user/{userId} and /admin/user/{userId}

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -13,7 +13,8 @@ import java.io.Serializable;
 @NamedQueries(
         {
                 @NamedQuery(name = "userByName", query = "select u from UserEntity u where u.username = :username"),
-                @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email =:email")
+                @NamedQuery(name = "userByEmail", query = "select u from UserEntity u where u.email =:email"),
+                @NamedQuery(name = "userByUuid", query = "select u from UserEntity u where u.uuid =:uuid")
         }
 )
 public class UserEntity  implements Serializable {
