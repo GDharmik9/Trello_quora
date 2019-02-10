da68e5a6b2666e2e9b2249283431061be1a468c2
GhanDeveloper <GDg11104@gmail.com>
2019-02-10 18:19:28 +0530

    all type of error meassage and sucessed flag showed properly

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -37,14 +37,17 @@ public class QuestionBusinessService {
 
         QuestionEntity questionEntity = questionDao.getQuestionByUuid(questionId);
         UserEntity userEntity = userAuthEntity.getUser();
-        Integer userId = questionEntity.getUser().getId();
         String role = userEntity.getRole();
+
 
         if (userAuthEntity.getLogoutAt() != null) {
             throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to delete a question");
         } else if (questionEntity == null) {
             throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
-        } else if ((role.equals("nonadmin")) || !userEntity.getId().equals(userId)) {
+        }
+        Integer authUserId = userAuthEntity.getUser().getId();
+        Integer queUserId = questionEntity.getUser().getId();
+         if ((role.equals("nonadmin")) && (authUserId != queUserId)) {
                 throw new AuthorizationFailedException("ATHR-003", "Only the question owner or admin can delete the question");
         }
         return questionDao.deleteQuestion(questionEntity);
@@ -79,25 +82,22 @@ public class QuestionBusinessService {
     public QuestionEntity editQuestionContent(String questionId, UserAuthEntity userAuthEntity , String content) throws AuthorizationFailedException, InvalidQuestionException {
 
        QuestionEntity questionEntity = questionDao.getQuestionByUuid(questionId);
-        boolean validuser = compareUser(userAuthEntity, questionEntity);
+
+
         if (userAuthEntity.getLogoutAt() != null) {
             throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit the question");
         } else if (questionEntity == null) {
             throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
-        }else if (validuser==false){
+        }
+        Integer authUserId = userAuthEntity.getUser().getId();
+        Integer queUserId = questionEntity.getUser().getId();
+        if (authUserId != queUserId){
                 throw new AuthorizationFailedException("ATHR-003", "Only the question owner can edit the question");
         }
                  questionEntity.setContent(content);
         return  questionDao.editQuestion(questionEntity);
     }
 
-    private boolean compareUser(UserAuthEntity authEntity, QuestionEntity queEntity){
-        Integer authUserId = authEntity.getUser().getId();
-        Integer queUserId = queEntity.getUser().getId();
-        if (authUserId == queUserId){
-            return true;
-        }
-        return false;
-    }
+
 }
 
